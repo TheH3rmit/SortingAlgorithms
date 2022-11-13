0a35ea5d0fb61ff90533d1b3fc913c9fec513ece
TheFoolsMaster <95000621+TheFoolsMaster@users.noreply.github.com>
2022-11-13 21:05:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Projekt_2_vistula
+{
+    internal class Program
+    {
+        static int[] tablica = new int[20];
+        static void Main(string[] args)
+        {
+            //Projekt
+            //Generacja tablicy o wieklkosci 20 z losowymi numerami
+            Random random = new Random();
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                tablica[i] = random.Next(0, 101);
+            }
+            //Wyswietlanie tablicy
+            DisplayArray(tablica);
+
+            Console.WriteLine("Algorytm dziala");
+            //Algorytm babelkowy
+            int krok = 0;
+            int oldCounter = -1;
+            int n = 0;
+            int nPlus = 1;
+            bool done = true;
+            while (done)
+            {
+                //resetuje wartosci jesli wychodza poza array
+                if (nPlus > tablica.Length - 1)
+                {
+                    n = 0;
+                    nPlus = 1;
+                }
+                //sprawdzam czy 1 jest wieksza od drugiej i zamieniam
+                if (tablica[n] < tablica[nPlus])
+                {
+                    oldCounter++;
+                    if (tablica.Length < oldCounter)
+                    {
+                        done = false;
+                    }
+                }
+                else if (tablica[n] > tablica[nPlus])
+                {
+                    oldCounter = 0;
+                    int temp = tablica[n];
+                    tablica[n] = tablica[nPlus];
+                    tablica[nPlus] = temp;
+                    krok++;
+                }
+                n++;
+                nPlus++;
+            }
+            DisplayArray(tablica);
+        }
+        private static void DisplayArray(int[] tablica)
+        {
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                Console.WriteLine(tablica[i]);
+            }
+        }
+    }
+}
