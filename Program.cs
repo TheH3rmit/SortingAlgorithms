728e8e1bed44057a163618c8a40489380c7e60b2
TheFoolsMaster <95000621+TheFoolsMaster@users.noreply.github.com>
2022-11-13 22:13:24 +0100

    Adding BubbleSorting, RandomizeArrayValues methodes

--- Program.cs
+++ Program.cs
@@ -5,49 +5,66 @@
         static int[] bubbleArray = new int[20];
         static void Main(string[] args)
         {
-            Random random = new Random();
-            //Insetring random values to all slots of the bubbleArray
-            for (int i = 0; i < bubbleArray.Length; i++) 
-            {
-                bubbleArray[i] = random.Next(0, 101);
-            }
+            RandomizeArrayValues(bubbleArray);
             DisplayArray(bubbleArray);
 
+            BubbleSorting(bubbleArray);
+            DisplayArray(bubbleArray);
+        }
+
+        /// <summary>
+        /// Sorts the the values of 1d array via bubble sorting algorithm 
+        /// </summary>
+        /// <param name="array"></param>
+        private static void BubbleSorting(int[] array)
+        {
             Console.WriteLine("Sorting values via bubble sorting algorithm");
-            int switchCounter = 0; // tracks how many times numbers changed place
-            int noSwitchCounter = -1; //track since when the last switch of values were made
-                                      // when all values are sorted correctly this value is equal the array lenght
-            int n = 0;
-            bool done = true;
+            int switchCounter = 0;      // tracks how many times numbers changed place
+            int noSwitchCounter = -1;   // track since when the last switch of values were made
+                                        // when all values are sorted correctly this value is equal the array lenght
+            int n = 0;                  // points to the index of the array
+            bool done = true;           // ends the while loop when algorithm finishes sorting
             while (done)
             {
                 //Resets the value of the variable array index if it goes out of bounds of the bubbleArray
-                if (n + 1 > bubbleArray.Length - 1)
+                if (n + 1 > array.Length - 1)
                 {
                     n = 0;
                 }
-                
+
                 //Compares values from bubbleArray
-                if (bubbleArray[n] < bubbleArray[n+1])
+                if (array[n] < array[n + 1])
                 {
                     noSwitchCounter++;
-                    if (bubbleArray.Length < noSwitchCounter)
+                    if (array.Length < noSwitchCounter)
                     {
                         done = false;
                     }
                 }
-                else if (bubbleArray[n] > bubbleArray[n + 1])
+                else if (array[n] > array[n + 1])
                 {
                     noSwitchCounter = 0;
-                    int temp = bubbleArray[n];
-                    bubbleArray[n] = bubbleArray[n + 1];
-                    bubbleArray[n + 1] = temp;
+                    int temp = array[n];
+                    array[n] = array[n + 1];
+                    array[n + 1] = temp;
                     switchCounter++;
                 }
 
                 n++;
             }
-            DisplayArray(bubbleArray);
+        }
+
+        /// <summary>
+        /// Inserts random values to all slots of the array
+        /// </summary>
+        /// <param name="array"></param>
+        private static void RandomizeArrayValues(int[] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 101);
+            }
         }
 
         /// <summary>
